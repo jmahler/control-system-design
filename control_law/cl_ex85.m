ba42b2a49ef3173dc6d562ffcdb1f87dcedb09fa
Jeremiah Mahler <jmmahler@gmail.com>
2013-12-16 09:15:16 -0800

    plot of predictor along with non-predictor
    For the plot of the full order predictor estimator,
    the plot of the non-predictor has been overlaid
    for comparison.

--- control_law/cl_ex85.m
+++ control_law/cl_ex85.m
@@ -2,7 +2,8 @@
 % cl_ex85.m
 %
 % Control Law, Full Order, Predictor Estimator.
-% 
+% Predictor is show along with non-predictor.
+%
 % From Example 8.5 on Pg. 294 of Franklin & Powell.
 % Expansion of Example 8.1 and 8.2.
 %
@@ -34,22 +35,31 @@ z2 = [(0.4 + 0.4i) (0.4 - 0.4i)];
 Lp = place(Phi', H', z2)';
 sysL = ss((Phi - Gamma*K1 - Lp*H), Lp, eye(n), zeros(2,1), T);
 
-% Build system
+% Build system(s)
+% predicted
 sysCL1 = feedback(Gz*H, sysL*sysK1, -1);
 sysCL1 = sminreal(sysCL1);
+% full feedback
+sysCL2 = feedback(Gz, sysK1, -1);
+sysCL2 = sminreal(sysCL2);
 
 % Simulate
 x0 = [1; 0; 0; 0];  % initial x
 u = zeros([(Tend/T) 2]);
 [y1,t1] = lsim(sysCL1, u, [], x0);
+x0 = [1; 0];  % initial x
+u = zeros([(Tend/T) 1]);
+[y2,t2] = lsim(sysCL2, u, [], x0);
 
 % Plot
 figure(1);
-%stairs(t1, y1);  % all
-stairs(t1, y1(:,1));  % one
+[ts1,ys1] = stairs(t1, y1(:,1));
+[ts2,ys2] = stairs(t2, y2(:,1));
+plot(ts1,ys1,ts2,ys2);
 grid on;
 axis tight;
 title('Regulator Response, Full Order Predictor Estimator');
+legend('predictor', 'non-predictor');
 xlabel('time (sec)');
 ylabel('y');
 
